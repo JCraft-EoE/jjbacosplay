bb2493387202158ed34943bc2bed72cfb850e94e
Ayutac <fly.high.android@gmail.com>
2026-04-28 23:21:32 +0200

    Improved Anvil interactions.

--- common/src/main/java/com/jcraft_eoe/jjbacosplay/mixin/AnvilMenuMixin.java
+++ common/src/main/java/com/jcraft_eoe/jjbacosplay/mixin/AnvilMenuMixin.java
@@ -71,8 +71,7 @@ public abstract class AnvilMenuMixin  extends ItemCombinerMenu {
                 return;
             }
             // if the items are the same, the anvil should already have calculated a result
-            // if they are of the same tier only, no change should be needed
-            if (armor1.getMaterial() == armor2.getMaterial()) {
+            if (armor1 == armor2) {
                 return;
             }
             final CosplayItem<?> cosplayItem = CosplayItem.find(armor1);
