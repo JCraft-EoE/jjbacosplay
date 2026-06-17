cc8a681c0f7a36e2abce5a9ecf0100e14ae6537b
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2026-06-17 21:56:41 +0200

    Fix datagen dependency issues for addon devs

--- datagen/src/main/java/com/jcraft_eoe/jjbacosplay/datagen/JjbaCosplayDataGen.java
+++ datagen/src/main/java/com/jcraft_eoe/jjbacosplay/datagen/JjbaCosplayDataGen.java
@@ -1,11 +1,13 @@
 package com.jcraft_eoe.jjbacosplay.datagen;
 
+import com.jcraft_eoe.jjbacosplay.JjbaCosplay;
 import com.jcraft_eoe.jjbacosplay.datagen.provider.assets.JCModelProvider;
 import com.jcraft_eoe.jjbacosplay.datagen.provider.data.JCAdvancementProvider;
 import com.jcraft_eoe.jjbacosplay.datagen.provider.data.JCRecipeProvider;
 import com.jcraft_eoe.jjbacosplay.datagen.provider.data.JCTagProviders;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import org.jetbrains.annotations.Nullable;
 
 public class JjbaCosplayDataGen implements DataGeneratorEntrypoint {
     @Override
@@ -16,5 +18,10 @@ public class JjbaCosplayDataGen implements DataGeneratorEntrypoint {
         pack.addProvider(JCTagProviders.JMEntityTags::new);
         pack.addProvider(JCRecipeProvider::new);
         pack.addProvider(JCAdvancementProvider::new);
+    }
+
+    @Override
+    public @Nullable String getEffectiveModId() {
+        return JjbaCosplay.MOD_ID;
     }
 }
